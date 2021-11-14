d020a8a9e5298bdede7d542ada1d586c7ca35059
GrandPa Ron <75545155+grandparon@users.noreply.github.com>
2021-11-13 20:27:42 -0800

    Initial files for MakeCode project

---
+++ README.md
@@ -0,0 +1,31 @@
+
+> Open this page at [https://grandparon.github.io/check-keypad---for-arm-project/](https://grandparon.github.io/check-keypad---for-arm-project/)
+
+## Use as Extension
+
+This repository can be added as an **extension** in MakeCode.
+
+* open [https://makecode.microbit.org/](https://makecode.microbit.org/)
+* click on **New Project**
+* click on **Extensions** under the gearwheel menu
+* search for **https://github.com/grandparon/check-keypad---for-arm-project** and import
+
+## Edit this project ![Build status badge](https://github.com/grandparon/check-keypad---for-arm-project/workflows/MakeCode/badge.svg)
+
+To edit this repository in MakeCode.
+
+* open [https://makecode.microbit.org/](https://makecode.microbit.org/)
+* click on **Import** then click on **Import URL**
+* paste **https://github.com/grandparon/check-keypad---for-arm-project** and click import
+
+## Blocks preview
+
+This image shows the blocks code from the last commit in master.
+This image may take a few minutes to refresh.
+
+![A rendered view of the blocks](https://github.com/grandparon/check-keypad---for-arm-project/raw/master/.github/makecode/blocks.png)
+
+#### Metadata (used for search, rendering)
+
+* for PXT/microbit
+<script src="https://makecode.com/gh-pages-embed.js"></script><script>makeCodeRender("{{ site.makecode.home_url }}", "{{ site.github.owner_name }}/{{ site.github.repository_name }}");</script>
---
+++ main.ts
@@ -0,0 +1,64 @@
+// There are 12 keys on this 12 keys on this keypad.. We need numbers for the '*' key and the '#' key.
+// '*'  will be 10
+// '#'  will be 12
+// The "return" instruction will return the number inside the circle to the instruction that called the function.
+function read_keyboard () {
+    // P0 col with zero at bottom
+    pins.digitalWritePin(DigitalPin.P0, 1)
+    // P0 col with zero at bottom
+    pins.digitalWritePin(DigitalPin.P16, 0)
+    // P0 col with zero at bottom
+    pins.digitalWritePin(DigitalPin.P8, 0)
+    // P1 row with the 4 on it
+    // P1
+    // P1
+    if (pins.digitalReadPin(DigitalPin.P1) == 1) {
+        return 5
+    } else if (pins.digitalReadPin(DigitalPin.P2) == 1) {
+        return 2
+    } else if (pins.digitalReadPin(DigitalPin.P12) == 1) {
+        return 8
+    } else if (pins.digitalReadPin(DigitalPin.P13) == 1) {
+        return 0
+    }
+    // P0 col with zero at bottom
+    pins.digitalWritePin(DigitalPin.P0, 0)
+    // set col 147 (pin 16) to one and read row 123 (pin  2) then row 456 (pin 1) then row 789 (pin 12)
+    pins.digitalWritePin(DigitalPin.P16, 1)
+    // P1 row with the 4 on it
+    if (pins.digitalReadPin(DigitalPin.P1) == 1) {
+        return 4
+    } else if (pins.digitalReadPin(DigitalPin.P2) == 1) {
+        return 1
+    } else if (pins.digitalReadPin(DigitalPin.P12) == 1) {
+        return 7
+    } else if (pins.digitalReadPin(DigitalPin.P13) == 1) {
+        // same as * key
+        return 10
+    }
+    // set col 147 (pin 16) to one and read row 123 (pin  2) then row 456 (pin 1) then row 789 (pin 12)
+    pins.digitalWritePin(DigitalPin.P16, 0)
+    // P0 col with zero at bottom
+    pins.digitalWritePin(DigitalPin.P8, 1)
+    // P1 row with the 4 on it
+    if (pins.digitalReadPin(DigitalPin.P1) == 1) {
+        return 6
+    } else if (pins.digitalReadPin(DigitalPin.P2) == 1) {
+        return 3
+    } else if (pins.digitalReadPin(DigitalPin.P12) == 1) {
+        return 9
+    } else if (pins.digitalReadPin(DigitalPin.P13) == 1) {
+        // same as # key
+        return 11
+    }
+    // P0 col with zero at bottom
+    pins.digitalWritePin(DigitalPin.P8, 0)
+    return 99
+}
+basic.forever(function () {
+    if (99 != read_keyboard()) {
+        basic.showNumber(read_keyboard())
+        basic.pause(100)
+        basic.clearScreen()
+    }
+})
